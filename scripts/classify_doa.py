e78b86c488c755717dd6833187a183038e184f7e
Theresa Cheng <tcheng@uoregon.edu>
2020-01-28 19:02:47 -0800

    implement leave 2 out, save model info

--- scripts/classify_doa.py
+++ scripts/classify_doa.py
@@ -4,10 +4,12 @@
 import csv
 import pyreadr
 import numpy as np
-import statistics as stats
+#import statistics as stats
+import scipy.stats as stats
+from itertools import compress
 from random import sample
 
-from sklearn.model_selection import LeaveOneOut, train_test_split, GridSearchCV, cross_val_score
+from sklearn.model_selection import LeaveOneOut, StratifiedKFold, train_test_split, GridSearchCV, cross_val_score
 from sklearn.utils import Bunch
 
 loo = LeaveOneOut()
@@ -18,6 +20,15 @@ from sklearn.feature_selection import SelectPercentile, f_classif
 
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# define some list extraction functions
+def extract(lst):
+    return [item[0] for item in lst]
+
+
+def extract_two(lst):
+    return [item[1] for item in lst]
+
 
 from sklearn.pipeline import make_pipeline
 run_permutation = False # this takes ~ 1 hr
@@ -85,13 +96,14 @@ loo.get_n_splits(classify_adv_df.data)
 
 # fit models
 svc = LinearSVC()
-percent_of_features = 15
+percent_of_features = 30
 feat_sel = SelectPercentile(f_classif, percent_of_features)
 
 y_pred_all = []
 y_test_all = []
+y_dec_func_all = []
 n_features = round((percent_of_features/100)*len(classify_adv_df.data[0]))
-feat_sel_idxes = np.empty((len(classify_adv_df.data), n_features), dtype=np.int8) #(
+#feat_sel_idxes = np.empty((len(classify_adv_df.data), n_features), dtype=np.int8) #(
 
 for train_index, test_index in loo.split(classify_adv_df.data):
    # print("TRAIN:", train_index, "TEST:", test_index)
@@ -108,12 +120,27 @@ for train_index, test_index in loo.split(classify_adv_df.data):
 
     # fit model with pared down training set
     svc.fit(X_train_new, y_train)
+    y_dec_func_all.append(svc.decision_function(X_test_new))
     y_pred_all.append(svc.predict(X_test_new)) # predict outcomes based on new test set
     y_test_all.append(y_test)
 
 adv_tn, adv_fp, adv_fn, adv_tp = confusion_matrix(y_test_all, y_pred_all).ravel()
 pred_adv_accuracy = (adv_tn + adv_tp)/(adv_tn + adv_tp + adv_fn + adv_fp)
 print('Adversity status accuracy:', round(pred_adv_accuracy, 2))
+
+# compare classification confidence in the true adversity and no adversity groups
+y_dec_func_adv = list(compress(y_dec_func_all, classify_adv_df.target))
+y_dec_func_no_adv = list(compress(y_dec_func_all, np.logical_not(classify_adv_df.target)))
+
+ttest_result = stats.ttest_ind(y_dec_func_adv, y_dec_func_no_adv)
+
+# save subject_id, study, protocol, adv, abuse, neglect, class_label, class_evidence, mean_FD, mean_FD_censored
+anon_ids = ["anon_" + str(num) for num in range(1,len(beh.study)+1)]
+
+with open('output/adv_classifier_by_subj.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["subject_id", "study", "protocol", "adv", "abuse", "neglect", "adv_pred", "adv_dec_func", "mean_FD", "mean_FD_censored"])
+    writer.writerows(zip(anon_ids, beh.study, beh.protocol, beh.maltreatment, beh.abuse, beh.neglect, y_pred_all, y_dec_func_all, beh.mean_FD, beh.mean_FD_censored))
 
 # plt.hist(feat_sel_idxes, bins=91)
 
@@ -185,8 +212,13 @@ none_idx = np.asarray(none_idx)[0]
 # convert pandas dataframe into numpy array, and then delete and reconstitute
 np_beh_values = beh.values
 
-# re-run classifier 1000x classifying abuse against a subset of none individuals, save accuracy values
+# CLASSIFY INDIVIDUALS WITH ABUSE
 all_abuse_accuracies = []
+all_abuse_tstats = []
+all_abuse_hit = []
+all_abuse_fa = []
+avg_y_dec_func_abuse = []
+avg_y_dec_func_none = []
 
 if run_sampling == True:
     for i in range(1000):
@@ -202,13 +234,17 @@ if run_sampling == True:
                                        target=np.asarray(abuse_none_beh.abuse.astype(int)),
                                        data=data[np.sort(np.concatenate([abuse_only_idx, none_idx_subsample]))])
 
+        skf = StratifiedKFold(n_splits=int(len(classify_abuse_df.data) / 2),
+                              shuffle=True)  # remove each participant 1x for a total of 33 folds, but shuffle so that one from each group is randomly selected
+        skf.get_n_splits(classify_abuse_df.data, classify_abuse_df.target)
+
         y_pred_abuse = []
         y_test_abuse = []
+        y_dec_func = []
+        test_indices = []
 
-        loo.get_n_splits(classify_abuse_df.data)
-
-        for train_index, test_index in loo.split(classify_abuse_df.data):
-            #print("TRAIN:", train_index, "TEST:", test_index)
+        for train_index, test_index in skf.split(X=classify_abuse_df.data, y=classify_abuse_df.target):
+            print("TRAIN:", train_index, "TEST:", test_index)
 
             # generate train and test feature and outcome sets
             X_train, X_test = classify_abuse_df.data[train_index], classify_abuse_df.data[test_index]
@@ -222,32 +258,64 @@ if run_sampling == True:
 
             # fit model with pared down training set
             svc.fit(X_train_new, y_train)
+            y_dec_func.append(svc.decision_function(X_test_new))
             y_pred_abuse.append(svc.predict(X_test_new))  # predict outcomes based on new test set
             y_test_abuse.append(y_test)
+            test_indices.append(test_index)
 
-        abuse_tn, abuse_fp, abuse_fn, abuse_tp = confusion_matrix(y_test_abuse, y_pred_abuse).ravel()
-        pred_abuse_accuracy = (abuse_tn + abuse_tp)/(abuse_tn + abuse_tp + abuse_fn + abuse_fp)
+        # need to unpack arrays of doubles by extracting and concatenating them
+        y_dec_func_all = extract(y_dec_func) + extract_two(y_dec_func)
+        y_test_abuse_all = extract(y_test_abuse) + extract_two(y_test_abuse)
+        y_pred_abuse_all = extract(y_pred_abuse) + extract_two(y_pred_abuse)
+        test_indices = extract(test_indices) + extract_two(test_indices)
 
-        all_abuse_accuracies.append(pred_abuse_accuracy)
+        # compare classification confidence in the true adversity and no adversity groups
+        y_dec_func_abuse = list(compress(y_dec_func_all, classify_abuse_df.target[test_indices]))
+        y_dec_func_none = list(compress(y_dec_func_all, np.logical_not(classify_abuse_df.target[test_indices])))
+
+        ttest_result = stats.ttest_ind(y_dec_func_abuse, y_dec_func_none)
+
+        # ok but this part should work
+        abuse_tn, abuse_fp, abuse_fn, abuse_tp = confusion_matrix(y_test_abuse_all, y_pred_abuse_all).ravel()
+        half_total = len(classify_abuse_df.target)/2
+        pred_abuse_accuracy = (abuse_tn + abuse_tp)/(abuse_tn + abuse_fp + abuse_fn + abuse_tp)
+
+        all_abuse_accuracies.append(round(pred_abuse_accuracy,2))
+        all_abuse_tstats.append(ttest_result[0])
+        all_abuse_hit.append(round(abuse_tp/half_total, 2))
+        all_abuse_fa.append(round(abuse_fp/half_total, 2))
+
+        avg_y_dec_func_abuse.append(round(np.mean(y_dec_func_abuse),2))
+        avg_y_dec_func_none.append(round(np.mean(y_dec_func_none),2))
 
     # plot a distribution of accuracy values
-    #plt.hist(all_abuse_accuracies, bins='auto')
-    #plt.show()
-    #plt.savefig('output/abuse_accuracies.png')
-    #plt.savefig('output/abuse_accuracies_hs_feat.png')
+    plt.hist(all_abuse_accuracies, bins='auto')
+    plt.savefig('output/abuse_accuracies.png')
+    plt.close()
 
-plt.close()
+    plt.hist(all_abuse_tstats, bins='auto')
+    plt.savefig('output/abuse_decfunc_tstats.png')
+    plt.close()
 
-print('Abuse status accuracy:', round(stats.mean(all_abuse_accuracies), 2))
+print('Abuse status accuracy:', round(np.mean(all_abuse_accuracies), 2))
 
-# re-run classifier 1000x with shuffled target labels and save accuracy values
+with open('output/abuse_classifier_perm.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["avg_decfunc_abuse", "avg_decfunc_none", "t_stat", "perc_hits", "per_fa", "accuracy"])
+    writer.writerows(zip(avg_y_dec_func_abuse, avg_y_dec_func_none, all_abuse_tstats, all_abuse_hit, all_abuse_fa, all_abuse_accuracies))
+
+# CLASSIFY INDIVIDUALS WITH NEGLECT
 all_neglect_accuracies = []
+all_neglect_tstats = []
+all_neglect_hit = []
+all_neglect_fa = []
+avg_y_dec_func_neglect = []
+avg_y_dec_func_none = []
 
 if run_sampling == True:
     for i in range(1000):
-        # take a random subset of the none_idx so that they match the number of individuals that have experienced abuse
+        # take a random subset of the none_idx so that they match the number of individuals that have experienced neglect
         none_idx_subsample = sample(list(none_idx), len(neglect_only_idx))
-
 
         # Train and test neglect classifier with loo
         ## neglect vs. none
@@ -258,13 +326,19 @@ if run_sampling == True:
                                        target=np.asarray(neglect_none_beh.neglect.astype(int)),
                                        data=data[np.sort(np.concatenate([neglect_only_idx, none_idx_subsample]))])
 
+        skf = StratifiedKFold(n_splits=int(len(classify_neglect_df.data) / 2),
+                              shuffle=True)  # remove each participant 1x for a total of 33 folds, but shuffle so that one from each group is randomly selected
+        skf.get_n_splits(classify_neglect_df.data, classify_neglect_df.target)
+
         y_pred_neglect = []
         y_test_neglect = []
+        y_dec_func = []
+        test_indices = []
 
-        loo.get_n_splits(classify_neglect_df.data)
+        for train_index, test_index in skf.split(X=classify_neglect_df.data, y=classify_neglect_df.target):
+            print("TRAIN:", train_index, "TEST:", test_index)
 
-        for train_index, test_index in loo.split(classify_neglect_df.data):
-           # print("TRAIN:", train_index, "TEST:", test_index)
+            # generate train and test feature and outcome sets
             X_train, X_test = classify_neglect_df.data[train_index], classify_neglect_df.data[test_index]
             y_train, y_test = classify_neglect_df.target[train_index], classify_neglect_df.target[test_index]
 
@@ -274,49 +348,90 @@ if run_sampling == True:
             X_test_new = feat_sel.transform(X_test)  # pare down test set features
             #feat_sel_idxes[test_index] = feat_sel.get_support(indices=True)
 
+            # fit model with pared down training set
             svc.fit(X_train_new, y_train)
-            y_pred_neglect.append(svc.predict(X_test_new))
+            y_dec_func.append(svc.decision_function(X_test_new))
+            y_pred_neglect.append(svc.predict(X_test_new))  # predict outcomes based on new test set
             y_test_neglect.append(y_test)
+            test_indices.append(test_index)
 
-        neglect_tn, neglect_fp, neglect_fn, neglect_tp = confusion_matrix(y_test_neglect, y_pred_neglect).ravel()
-        pred_neglect_accuracy = (neglect_tn + neglect_tp)/(neglect_tn + neglect_tp + neglect_fn + neglect_fp)
+        # need to unpack arrays of doubles by extracting and concatenating them
+        y_dec_func_all = extract(y_dec_func) + extract_two(y_dec_func)
+        y_test_neglect_all = extract(y_test_neglect) + extract_two(y_test_neglect)
+        y_pred_neglect_all = extract(y_pred_neglect) + extract_two(y_pred_neglect)
+        test_indices = extract(test_indices) + extract_two(test_indices)
 
-        all_neglect_accuracies.append(pred_neglect_accuracy)
+        # compare classification confidence in the true adversity and no adversity groups
+        y_dec_func_neglect = list(compress(y_dec_func_all, classify_neglect_df.target[test_indices]))
+        y_dec_func_none = list(compress(y_dec_func_all, np.logical_not(classify_neglect_df.target[test_indices])))
+
+        ttest_result = stats.ttest_ind(y_dec_func_neglect, y_dec_func_none)
+
+        # ok but this part should work
+        neglect_tn, neglect_fp, neglect_fn, neglect_tp = confusion_matrix(y_test_neglect_all, y_pred_neglect_all).ravel()
+        half_total = len(classify_neglect_df.target)/2
+        pred_neglect_accuracy = (neglect_tn + neglect_tp)/(neglect_tn + neglect_fp + neglect_fn + neglect_tp)
+
+        all_neglect_accuracies.append(round(pred_neglect_accuracy,2))
+        all_neglect_tstats.append(ttest_result[0])
+        all_neglect_hit.append(round(neglect_tp/half_total, 2))
+        all_neglect_fa.append(round(neglect_fp/half_total, 2))
+
+        avg_y_dec_func_neglect.append(round(np.mean(y_dec_func_neglect),2))
+        avg_y_dec_func_none.append(round(np.mean(y_dec_func_none),2))
 
     # plot a distribution of accuracy values
-    #plt.hist(all_neglect_accuracies, bins='auto')
-    #plt.show()
-    #plt.savefig('output/neglect_accuracies.png')
-    #plt.savefig('output/neglect_accuracies_hs_feat.png')
+    plt.hist(all_neglect_accuracies, bins='auto')
+    plt.savefig('output/neglect_accuracies.png')
+    plt.close()
 
-#plt.close()
+    plt.hist(all_neglect_tstats, bins='auto')
+    plt.savefig('output/neglect_decfunc_tstats.png')
+    plt.close()
 
-print('Neglect status accuracy:', round(stats.mean(all_neglect_accuracies), 2))
+print('Neglect status accuracy:', round(np.mean(all_neglect_accuracies), 2))
+
+with open('output/neglect_classifier_perm.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["avg_decfunc_neglect", "avg_decfunc_none", "t_stat", "perc_hits", "per_fa", "accuracy"])
+    writer.writerows(zip(avg_y_dec_func_neglect, avg_y_dec_func_none, all_neglect_tstats, all_neglect_hit, all_neglect_fa, all_neglect_accuracies))
 
 
+# CLASSIFY INDIVIDUALS WITH BOTH
 all_both_accuracies = []
+all_both_tstats = []
+all_both_hit = []
+all_both_fa = []
+avg_y_dec_func_both = []
+avg_y_dec_func_none = []
 
 if run_sampling == True:
     for i in range(1000):
-        # take a random subset of the none_idx so that they match the number of individuals that have experienced abuse
-        none_idx_subsample = sample(list(none_idx), len(both_idx))
+        # take a random subset of the none_idx so that they match the number of individuals that have experienced both
+        none_idx_subsample = sample(list(none_idx), len(both_only_idx))
 
         # Train and test both classifier with loo
         ## both vs. none
-        both_none_beh_values = np_beh_values[np.sort(np.concatenate([both_idx, none_idx_subsample]))]
+        both_none_beh_values = np_beh_values[np.sort(np.concatenate([both_only_idx, none_idx_subsample]))]
         both_none_beh = pd.DataFrame(both_none_beh_values, columns = beh.columns)
         classify_both_df = base.Bunch(target_names='both',
                                        feature_names=feature_names,
-                                       target=np.asarray(both_none_beh.polyvictimization.astype(int)),
-                                       data=data[np.sort(np.concatenate([both_idx, none_idx_subsample]))])
+                                       target=np.asarray(both_none_beh.both.astype(int)),
+                                       data=data[np.sort(np.concatenate([both_only_idx, none_idx_subsample]))])
+
+        skf = StratifiedKFold(n_splits=int(len(classify_both_df.data) / 2),
+                              shuffle=True)  # remove each participant 1x for a total of 33 folds, but shuffle so that one from each group is randomly selected
+        skf.get_n_splits(classify_both_df.data, classify_both_df.target)
 
         y_pred_both = []
         y_test_both = []
+        y_dec_func = []
+        test_indices = []
 
-        loo.get_n_splits(classify_both_df.data)
+        for train_index, test_index in skf.split(X=classify_both_df.data, y=classify_both_df.target):
+            print("TRAIN:", train_index, "TEST:", test_index)
 
-        for train_index, test_index in loo.split(classify_both_df.data):
-            #print("TRAIN:", train_index, "TEST:", test_index)
+            # generate train and test feature and outcome sets
             X_train, X_test = classify_both_df.data[train_index], classify_both_df.data[test_index]
             y_train, y_test = classify_both_df.target[train_index], classify_both_df.target[test_index]
 
@@ -326,24 +441,53 @@ if run_sampling == True:
             X_test_new = feat_sel.transform(X_test)  # pare down test set features
             #feat_sel_idxes[test_index] = feat_sel.get_support(indices=True)
 
+            # fit model with pared down training set
             svc.fit(X_train_new, y_train)
-            y_pred_both.append(svc.predict(X_test_new))
+            y_dec_func.append(svc.decision_function(X_test_new))
+            y_pred_both.append(svc.predict(X_test_new))  # predict outcomes based on new test set
             y_test_both.append(y_test)
+            test_indices.append(test_index)
 
-        both_tn, both_fp, both_fn, both_tp = confusion_matrix(y_test_both, y_pred_both).ravel()
-        pred_both_accuracy = (both_tn + both_tp)/(both_tn + both_tp + both_fn + both_fp)
+        # need to unpack arrays of doubles by extracting and concatenating them
+        y_dec_func_all = extract(y_dec_func) + extract_two(y_dec_func)
+        y_test_both_all = extract(y_test_both) + extract_two(y_test_both)
+        y_pred_both_all = extract(y_pred_both) + extract_two(y_pred_both)
+        test_indices = extract(test_indices) + extract_two(test_indices)
 
-        all_both_accuracies.append(pred_both_accuracy)
+        # compare classification confidence in the true adversity and no adversity groups
+        y_dec_func_both = list(compress(y_dec_func_all, classify_both_df.target[test_indices]))
+        y_dec_func_none = list(compress(y_dec_func_all, np.logical_not(classify_both_df.target[test_indices])))
+
+        ttest_result = stats.ttest_ind(y_dec_func_both, y_dec_func_none)
+
+        # ok but this part should work
+        both_tn, both_fp, both_fn, both_tp = confusion_matrix(y_test_both_all, y_pred_both_all).ravel()
+        half_total = len(classify_both_df.target)/2
+        pred_both_accuracy = (both_tn + both_tp)/(both_tn + both_fp + both_fn + both_tp)
+
+        all_both_accuracies.append(round(pred_both_accuracy,2))
+        all_both_tstats.append(ttest_result[0])
+        all_both_hit.append(round(both_tp/half_total, 2))
+        all_both_fa.append(round(both_fp/half_total, 2))
+
+        avg_y_dec_func_both.append(round(np.mean(y_dec_func_both),2))
+        avg_y_dec_func_none.append(round(np.mean(y_dec_func_none),2))
 
     # plot a distribution of accuracy values
-    #plt.hist(all_both_accuracies, bins='auto')
-    #plt.show()
-    #plt.savefig('output/both_accuracies.png')
-    #plt.savefig('output/both_accuracies_hs_feat.png')
+    plt.hist(all_both_accuracies, bins='auto')
+    plt.savefig('output/both_accuracies.png')
+    plt.close()
 
-plt.close()
+    plt.hist(all_both_tstats, bins='auto')
+    plt.savefig('output/both_decfunc_tstats.png')
+    plt.close()
 
-print('Both status accuracy:', round(stats.mean(all_both_accuracies), 2))
+print('Both status accuracy:', round(np.mean(all_both_accuracies), 2))
+
+with open('output/both_classifier_perm.csv', 'w') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["avg_decfunc_both", "avg_decfunc_none", "t_stat", "perc_hits", "per_fa", "accuracy"])
+    writer.writerows(zip(avg_y_dec_func_both, avg_y_dec_func_none, all_both_tstats, all_both_hit, all_both_fa, all_both_accuracies))
 
 ## CROSS-CATEGORY - NOTE: haven't implemented the random sampling, since there doesn't seem to be a point if it can't predict itself
 
